f9a26bca493771918be8601ea16e27d7c39093e4
dipubadatya <148934509+dipubadatya@users.noreply.github.com>
2025-04-05 21:04:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -62,11 +62,10 @@ const sessionOption = {
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure:true,
         httpOnly:true,
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
+    
     },
 };
 

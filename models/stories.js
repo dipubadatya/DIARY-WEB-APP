b3403fa3870756da31e2ee2a8b742f5d974c0cae
dipubadatya <148934509+dipubadatya@users.noreply.github.com>
2025-07-07 12:09:51 +0530

    Update stories.js

--- models/stories.js
+++ models/stories.js
@@ -1,19 +1,19 @@
 const { timeStamp } = require('console')
 const mongoose = require('mongoose')
 const { type } = require('os')
-const Comment=require('./comment.js') 
+const Comment = require('./comment.js')
 
 
 
 const storySchema = mongoose.Schema({
-       title: {
-              type: String,
-              Required: true
-       },
-       image: {
-              url:String,
-              filename:String,
-             publicId:String
+    title: {
+        type: String,
+        Required: true
+    },
+    image: {
+        url: String,
+        filename: String,
+        publicId: String
 
        },
        story: {
@@ -23,13 +23,15 @@ const storySchema = mongoose.Schema({
               type: Date,
              default: Date.now
 
-          },
-          editedAt:{
-                type:Date,
-          },
-     category:{
-        type:String,
-        enum:[
+    },
+
+    editedAt: {
+        type: Date,
+    },
+
+    category: {
+        type: String,
+        enum: [
             'fantasy',
             'random-thoughts',
             'poetry',
@@ -45,39 +47,37 @@ const storySchema = mongoose.Schema({
             'children',
             'fanfiction',
             'other',
-               
+
         ]
-     },
-       owner: {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "User",
-       },
-      
-           likedBy:[{ type: mongoose.Schema.Types.ObjectId,ref: "User"}],
-           likesCounts:{type:Number, default:0 },
-            
-             
-         
-      
-       views: [
-              {
-                  type: mongoose.Schema.Types.ObjectId,
-                  ref: "User", 
-              }
-          ],
-          comments: [
-              {
-                  type: mongoose.Schema.Types.ObjectId,
-                  ref: "Comment", 
-              }
-          ],
-        
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+
+    likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    likesCounts: { type: Number, default: 0 },
+
+    views: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        }
+    ],
+
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment",
+        }
+    ],
+
 })
 
 // Middleware to remove the story reference from the user's stories array
 storySchema.post('findOneAndDelete', async function (story) {
-    if (story) {          
-   
+    if (story) {
+
         await mongoose.model('User').updateOne(
             { _id: story.owner },
             { $pull: { stories: story._id } }
